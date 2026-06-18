700abd9d25563dabbc7cf3074691ab9d43f60f81
myoun99 <shoppingmyoun@gmail.com>
2026-06-19 00:38:23 +0900

    Extract timeline frame cells row widget

--- lib/src/ui/timeline/layer_timeline_grid.dart
+++ lib/src/ui/timeline/layer_timeline_grid.dart
@@ -6,16 +6,12 @@ import '../../models/layer.dart';
 import '../../models/layer_kind.dart';
 import '../../models/layer_id.dart';
 import 'timeline_cell_exposure_state.dart';
-import 'timeline_cell_style.dart';
-import 'timeline_exposure_block_visual.dart';
-import 'selected_exposure_display_range_policy.dart';
+import 'timeline_frame_cells_row.dart';
 import 'timeline_frame_coordinate_policy.dart';
 import 'timeline_frame_range_policy.dart';
-import 'timeline_frame_cell.dart';
 import 'timeline_frame_ruler.dart';
 import 'timeline_grid_metrics.dart';
 import 'timeline_horizontal_offset_policy.dart';
-import 'timeline_selected_exposure_outline.dart';
 import 'timeline_panel_virtualization_adapter.dart';
 import 'timeline_playhead.dart';
 
@@ -521,7 +517,7 @@ class _LayerTimelineGridState extends State<LayerTimelineGrid> {
                                                                 for (final layer
                                                                     in widget
                                                                         .layers)
-                                                                  _FrameCellsRow(
+                                                                  TimelineFrameCellsRow(
                                                                     layer:
                                                                         layer,
                                                                     active:
@@ -545,6 +541,9 @@ class _LayerTimelineGridState extends State<LayerTimelineGrid> {
                                                                         plan.leadingFrameSpacerWidth,
                                                                     trailingFrameSpacerWidth:
                                                                         plan.trailingFrameSpacerWidth,
+                                                                    metrics:
+                                                                        LayerTimelineGrid
+                                                                            ._metrics,
                                                                     exposureStateForLayer:
                                                                         widget
                                                                             .exposureStateForLayer,
@@ -1142,112 +1141,6 @@ class _LayerControlsRow extends StatelessWidget {
           ),
         ),
       ),
-    );
-  }
-}
-
-class _FrameCellsRow extends StatelessWidget {
-  const _FrameCellsRow({
-    required this.layer,
-    required this.active,
-    required this.currentFrameIndex,
-    required this.playbackFrameCount,
-    required this.frameStartIndex,
-    required this.frameEndIndexExclusive,
-    required this.leadingFrameSpacerWidth,
-    required this.trailingFrameSpacerWidth,
-    required this.exposureStateForLayer,
-    this.hasMarkForLayer,
-    this.frameNameForLayer,
-    required this.onSelectLayer,
-    required this.onSelectFrame,
-  });
-
-  final Layer layer;
-  final bool active;
-  final int currentFrameIndex;
-  final int playbackFrameCount;
-  final int frameStartIndex;
-  final int frameEndIndexExclusive;
-  final double leadingFrameSpacerWidth;
-  final double trailingFrameSpacerWidth;
-  final TimelineCellExposureState Function(Layer layer, int frameIndex)
-  exposureStateForLayer;
-  final bool Function(Layer layer, int frameIndex)? hasMarkForLayer;
-  final String? Function(Layer layer, int frameIndex)? frameNameForLayer;
-  final ValueChanged<LayerId> onSelectLayer;
-  final ValueChanged<int> onSelectFrame;
-
-  @override
-  Widget build(BuildContext context) {
-    final selectedExposureDisplayRange = resolveSelectedExposureDisplayRange(
-      active: active,
-      currentFrameIndex: currentFrameIndex,
-      frameStartIndex: frameStartIndex,
-      frameEndIndexExclusive: frameEndIndexExclusive,
-      exposureStateAt: (frameIndex) => exposureStateForLayer(layer, frameIndex),
-    );
-    final selectedExposureRange = selectedExposureDisplayRange.resolvedRange;
-    return Stack(
-      key: ValueKey<String>('timeline-frame-row-area-${layer.id}'),
-      children: [
-        Row(
-          children: [
-            SizedBox(
-              key: ValueKey<String>(
-                'timeline-frame-row-leading-spacer-${layer.id}',
-              ),
-              width: leadingFrameSpacerWidth,
-              height: LayerTimelineGrid._metrics.layerRowHeight,
-            ),
-            for (
-              var frameIndex = frameStartIndex;
-              frameIndex < frameEndIndexExclusive;
-              frameIndex += 1
-            )
-              TimelineFrameCell(
-                layer: layer,
-                frameIndex: frameIndex,
-                active: active,
-                selected: active && frameIndex == currentFrameIndex,
-                outsidePlaybackRange: frameIndex >= playbackFrameCount,
-                exposureState: exposureStateForLayer(layer, frameIndex),
-                selectedExposureRangeSegment:
-                    frameIndex >= selectedExposureRange.startFrameIndex &&
-                    frameIndex < selectedExposureRange.endFrameIndexExclusive,
-                exposureBlockSegment:
-                    calculateTimelineExposureBlockVisualSegment(
-                      previous: frameIndex == 0
-                          ? null
-                          : exposureStateForLayer(layer, frameIndex - 1),
-                      current: exposureStateForLayer(layer, frameIndex),
-                      next: exposureStateForLayer(layer, frameIndex + 1),
-                    ),
-                hasMark: hasMarkForLayer?.call(layer, frameIndex) ?? false,
-                frameName: frameNameForLayer?.call(layer, frameIndex),
-                onSelectLayer: onSelectLayer,
-                onSelectFrame: onSelectFrame,
-              ),
-            SizedBox(
-              key: ValueKey<String>(
-                'timeline-frame-row-trailing-spacer-${layer.id}',
-              ),
-              width: trailingFrameSpacerWidth,
-              height: LayerTimelineGrid._metrics.layerRowHeight,
-            ),
-          ],
-        ),
-        TimelineSelectedExposureOutline(
-          layerId: layer.id,
-          displayRange: selectedExposureDisplayRange,
-          frameStartIndex: frameStartIndex,
-          leadingFrameSpacerWidth: leadingFrameSpacerWidth,
-          frameCellWidth: LayerTimelineGrid._metrics.frameCellWidth,
-          rowHeight: LayerTimelineGrid._metrics.layerRowHeight,
-          borderColor: timelineSelectedFrameBorderColor,
-          borderRadius: const BorderRadius.all(Radius.circular(6)),
-        ),
-      ],
     );
   }
 }
---
+++ lib/src/ui/timeline/timeline_frame_cells_row.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+import '../../models/layer.dart';
+import '../../models/layer_id.dart';
+import 'selected_exposure_display_range_policy.dart';
+import 'timeline_cell_exposure_state.dart';
+import 'timeline_cell_style.dart';
+import 'timeline_exposure_block_visual.dart';
+import 'timeline_frame_cell.dart';
+import 'timeline_grid_metrics.dart';
+import 'timeline_selected_exposure_outline.dart';
+
+class TimelineFrameCellsRow extends StatelessWidget {
+  const TimelineFrameCellsRow({
+    required this.layer,
+    required this.active,
+    required this.currentFrameIndex,
+    required this.playbackFrameCount,
+    required this.frameStartIndex,
+    required this.frameEndIndexExclusive,
+    required this.leadingFrameSpacerWidth,
+    required this.trailingFrameSpacerWidth,
+    required this.metrics,
+    required this.exposureStateForLayer,
+    this.hasMarkForLayer,
+    this.frameNameForLayer,
+    required this.onSelectLayer,
+    required this.onSelectFrame,
+  });
+
+  final Layer layer;
+  final bool active;
+  final int currentFrameIndex;
+  final int playbackFrameCount;
+  final int frameStartIndex;
+  final int frameEndIndexExclusive;
+  final double leadingFrameSpacerWidth;
+  final double trailingFrameSpacerWidth;
+  final TimelineGridMetrics metrics;
+  final TimelineCellExposureState Function(Layer layer, int frameIndex)
+      exposureStateForLayer;
+  final bool Function(Layer layer, int frameIndex)? hasMarkForLayer;
+  final String? Function(Layer layer, int frameIndex)? frameNameForLayer;
+  final ValueChanged<LayerId> onSelectLayer;
+  final ValueChanged<int> onSelectFrame;
+
+  @override
+  Widget build(BuildContext context) {
+    final selectedExposureDisplayRange = resolveSelectedExposureDisplayRange(
+      active: active,
+      currentFrameIndex: currentFrameIndex,
+      frameStartIndex: frameStartIndex,
+      frameEndIndexExclusive: frameEndIndexExclusive,
+      exposureStateAt: (frameIndex) =>
+          exposureStateForLayer(layer, frameIndex),
+    );
+    final selectedExposureRange = selectedExposureDisplayRange.resolvedRange;
+    return Stack(
+      key: ValueKey<String>('timeline-frame-row-area-${layer.id}'),
+      children: [
+        Row(
+          children: [
+            SizedBox(
+              key: ValueKey<String>(
+                'timeline-frame-row-leading-spacer-${layer.id}',
+              ),
+              width: leadingFrameSpacerWidth,
+              height: metrics.layerRowHeight,
+            ),
+            for (
+              var frameIndex = frameStartIndex;
+              frameIndex < frameEndIndexExclusive;
+              frameIndex += 1
+            )
+              TimelineFrameCell(
+                layer: layer,
+                frameIndex: frameIndex,
+                active: active,
+                selected: active && frameIndex == currentFrameIndex,
+                outsidePlaybackRange: frameIndex >= playbackFrameCount,
+                exposureState: exposureStateForLayer(layer, frameIndex),
+                selectedExposureRangeSegment:
+                    frameIndex >= selectedExposureRange.startFrameIndex &&
+                    frameIndex < selectedExposureRange.endFrameIndexExclusive,
+                exposureBlockSegment:
+                    calculateTimelineExposureBlockVisualSegment(
+                      previous: frameIndex == 0
+                          ? null
+                          : exposureStateForLayer(layer, frameIndex - 1),
+                      current: exposureStateForLayer(layer, frameIndex),
+                      next: exposureStateForLayer(layer, frameIndex + 1),
+                    ),
+                hasMark: hasMarkForLayer?.call(layer, frameIndex) ?? false,
+                frameName: frameNameForLayer?.call(layer, frameIndex),
+                onSelectLayer: onSelectLayer,
+                onSelectFrame: onSelectFrame,
+              ),
+            SizedBox(
+              key: ValueKey<String>(
+                'timeline-frame-row-trailing-spacer-${layer.id}',
+              ),
+              width: trailingFrameSpacerWidth,
+              height: metrics.layerRowHeight,
+            ),
+          ],
+        ),
+        TimelineSelectedExposureOutline(
+          layerId: layer.id,
+          displayRange: selectedExposureDisplayRange,
+          frameStartIndex: frameStartIndex,
+          leadingFrameSpacerWidth: leadingFrameSpacerWidth,
+          frameCellWidth: metrics.frameCellWidth,
+          rowHeight: metrics.layerRowHeight,
+          borderColor: timelineSelectedFrameBorderColor,
+          borderRadius: const BorderRadius.all(Radius.circular(6)),
+        ),
+      ],
+    );
+  }
+}
