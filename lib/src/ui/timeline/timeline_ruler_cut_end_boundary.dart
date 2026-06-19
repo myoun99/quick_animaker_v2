4d9a21a807d511415ef7e4ce28f2221b62646126
myoun99 <shoppingmyoun@gmail.com>
2026-06-19 17:31:29 +0900

    Extract timeline ruler cut-end boundary

---
+++ lib/src/ui/timeline/timeline_ruler_cut_end_boundary.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TimelineRulerCutEndBoundary extends StatelessWidget {
+  const TimelineRulerCutEndBoundary({
+    super.key = const ValueKey<String>('timeline-cut-end-boundary-ruler'),
+    required this.left,
+  });
+
+  final double left;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      left: left,
+      top: 0,
+      bottom: 0,
+      width: 2,
+      child: const IgnorePointer(
+        child: DecoratedBox(decoration: BoxDecoration(color: Colors.red)),
+      ),
+    );
+  }
+}
