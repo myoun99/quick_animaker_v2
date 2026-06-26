d8c451dea55c12776d38fead363d1c09122d346a
myoun99 <shoppingmyoun@gmail.com>
2026-06-27 00:09:26 +0900

    Add Brush V1 integration review guards

--- test/ui/brush_canvas_test_helpers.dart
+++ test/ui/brush_canvas_test_helpers.dart
@@ -1,5 +1,29 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:quick_animaker_v2/src/models/frame_composite_cache_key.dart';
+import 'package:quick_animaker_v2/src/models/layer_tile_cache_key.dart';
+import 'package:quick_animaker_v2/src/models/playback_preview_cache_key.dart';
+import 'package:quick_animaker_v2/src/services/cache_invalidation_executor.dart';
 import 'package:quick_animaker_v2/src/ui/canvas/interactive_brush_edit_canvas_view.dart';
+
+class FakeCacheInvalidationSink implements CacheInvalidationSink {
+  final layerTiles = <LayerTileCacheKey>[];
+  final frameComposites = <FrameCompositeCacheKey>[];
+  final playbackPreviews = <PlaybackPreviewCacheKey>[];
+
+  int get totalCalls =>
+      layerTiles.length + frameComposites.length + playbackPreviews.length;
+
+  @override
+  void invalidateLayerTile(LayerTileCacheKey key) => layerTiles.add(key);
+
+  @override
+  void invalidateFrameComposite(FrameCompositeCacheKey key) =>
+      frameComposites.add(key);
+
+  @override
+  void invalidatePlaybackPreview(PlaybackPreviewCacheKey key) =>
+      playbackPreviews.add(key);
+}
 
 Finder interactiveBrushCanvasFinder() =>
     find.byType(InteractiveBrushEditCanvasView);
